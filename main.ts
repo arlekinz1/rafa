5c5689f31717d39291d5a2879f76913ead560184
arlekinz1 <109761993+arlekinz1@users.noreply.github.com>
2022-07-21 14:32:53 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
